045052d9872ae92e46f6336e19943e64ecd31920
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-07-07 16:46:44 +0100

    Try to add some html

--- server.js
+++ server.js
@@ -10,5 +10,22 @@ server.use("/foo", (req, res) => {
   return res.json({ foo: "bar" });
 });
 
+server.get("/", (req, res) => {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+      <header>
+      <h1>SUCCESS!</h1>
+      </header>
+    </body>
+    </html>
+    `;
+});
+
 server.use("/", mocks.server(server.Router(), false, false));
 server.start();
